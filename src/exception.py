9387f49ad92c5dbfa5f27f3e8acaf2c58b81ce35
bhargav7806 <bhargavjdv78@gmail.com>
2025-12-31 15:50:32 +0530

    this is commit

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error , error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -21,4 +21,10 @@ class CustomException(Exception):
         return self.error_message
     
 
+if __name__=="__main__":
+    try:
+        a = 10/0
+    except Exception as e:
+        logging.info("Devided by 0")
+        raise CustomException(e , sys)
 
--- src/logger.py
+++ src/logger.py
@@ -1,6 +1,8 @@
 import logging 
 import os 
 from datetime import datetime
+
+
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path = os.path.join(os.getcwd() , "logs"  , LOG_FILE)
 os.makedirs(logs_path , exist_ok= True)
@@ -14,3 +16,5 @@ logging.basicConfig(
     
 )
 
+if __name__ == "__main__":
+    logging.info("Logging has started")
